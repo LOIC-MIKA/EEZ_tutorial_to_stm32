4754fba7363442de3112f3dadb9ebae18aed0d01
LOIC-MIKA <128399334+LOIC-MIKA@users.noreply.github.com>
2024-10-12 22:21:37 +0800

    Add files via upload

--- ui/images.c
+++ ui/images.c
@@ -1,6 +1,9 @@
 #include "images.h"
 
-const ext_img_desc_t images[2] = {
+const ext_img_desc_t images[5] = {
     { "wallpaper", &img_wallpaper },
     { "user", &img_user },
+    { "WY", &img_wy },
+    { "mylike_un", &img_mylike_un },
+    { "mylike_pr", &img_mylike_pr },
 };
--- ui/images.h
+++ ui/images.h
@@ -9,6 +9,9 @@ extern "C" {
 
 extern const lv_img_dsc_t img_wallpaper;
 extern const lv_img_dsc_t img_user;
+extern const lv_img_dsc_t img_wy;
+extern const lv_img_dsc_t img_mylike_un;
+extern const lv_img_dsc_t img_mylike_pr;
 
 #ifndef EXT_IMG_DESC_T
 #define EXT_IMG_DESC_T
@@ -18,7 +21,7 @@ typedef struct _ext_img_desc_t {
 } ext_img_desc_t;
 #endif
 
-extern const ext_img_desc_t images[2];
+extern const ext_img_desc_t images[5];
 
 
 #ifdef __cplusplus
